fa2b6ebb80dc4c5da82ace8c6081e42463d99eed
oowsxq <oowsxq@user.noreply.github.com>
2020-06-04 17:40:23 +0800

    expose inner exception to outside

--- sql_engine/__init__.py
+++ sql_engine/__init__.py
@@ -1,2 +1,3 @@
-from .sql_engine import  SqlEngine
+from .sql_engine import SqlEngine
 from .sql_engine import Code
+from .sql_engine import SqlSyntaxException, SqlColumnException, ValueInvalidException
